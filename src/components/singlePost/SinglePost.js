b6e43d468294f5d3c9dfbd4083cc121246b2736a
Sujeong Ji <suji.sujeongji@gmail.com>
2022-07-08 23:56:01 +0900

    add a single post page

---
+++ src/components/singlePost/SinglePost.js
@@ -0,0 +1,11 @@
+import "./SinglePost.css"
+
+const SinglePost = () => {
+    return (
+        <div className='singlepost'>
+            single post
+        </div>
+    );
+}
+
+export default SinglePost;
