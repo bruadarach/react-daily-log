3bb6575d4aac9d59e0595247eebf6a294005095f
Sujeong Ji <suji.sujeongji@gmail.com>
2022-07-10 23:58:27 +0900

    add write component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import TopBar from "./components/topbar/TopBar"
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <>
     <TopBar/>
     {/* <Home /> */}
-    <Single />
+    {/* <Single /> */}
+    <Write />
     </>
   );
 }
---
+++ src/pages/write/Write.js
@@ -0,0 +1,26 @@
+import './Write.css';
+
+const Write = () => {
+    return (
+        <div className="write">
+            <img 
+                className='writeImg'
+                src="img/post-image.jpg" 
+                alt="write-image" 
+            />
+             <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput"><i className="writeIcon fas fa-plus"></i></label>
+                    <input type="file" id="fileInput" style={{display:"none"}}/>
+                    <input type="text" placeholder='Title' className="writeInput" autoFocus={true}/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder='Tell your story...' type="text" className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+             </form>
+        </div>
+    );
+}
+
+export default Write;
