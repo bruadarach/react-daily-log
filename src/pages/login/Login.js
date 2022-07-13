e15329d17f3dcd737b1bccd01c5083e5980dd9f9
Sujeong Ji <suji.sujeongji@gmail.com>
2022-07-13 22:31:53 +0900

    add login component

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,14 +3,15 @@ import './Login.css'
 const Login = () => {
     return (
         <div className="login">
+            <span className="loginTitle">Login</span>
             <form className="loginForm">
                 <label htmlFor="Email">Email</label>
-                <input type="text" placeholder='Enter your email'/>
+                <input className="loginInput" type="text" placeholder='Enter your email'/>
                 <label htmlFor="Email">Password</label>
-                <input type="password" placeholder='Enter your password'/>
+                <input className="loginInput" type="password" placeholder='Enter your password'/>
                 <button className="loginButton">Login</button>
             </form>
-            <button className='loginRegisterButton'>Register</button>
+            <button className='loginRegisterButton'>Sign Up</button>
         </div>
     );
 }
