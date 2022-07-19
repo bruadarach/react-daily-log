45105c55404bda1f6498093a6e0d69bac7fb618b
Sujeong Ji <suji.sujeongji@gmail.com>
2022-07-19 23:52:59 +0900

    set up routes

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 
 function App() {
   return (
-    <>
     <BrowserRouter>
-    <TopBar/>
-      <Routes>
-        <Route path ="/" element={<Home />} />
-        <Route path ="/register" element={<Register />} />
-        <Route path ="/register" element={<Register />} />
-        <Route path ="/login" element={<Login />} /> 
-        <Route path ="/write" element={<Write />} /> 
-        <Route path ="/settings" element={<Settings />} /> 
-        <Route path ="/post/:postId" element={<Single />} /> 
-      </Routes>
+      <TopBar/>
+      <Home />
+        <Routes>
+          <Route path ="/" element={<Home />} />
+          <Route path ="/register" element={<Register />} />
+          <Route path ="/register" element={<Register />} />
+          <Route path ="/login" element={<Login />} /> 
+          <Route path ="/write" element={<Write />} /> 
+          <Route path ="/settings" element={<Settings />} /> 
+          <Route path ="/post/:postId" element={<Single />} /> 
+        </Routes>
     </BrowserRouter>
-    </>
   );
 }
 
