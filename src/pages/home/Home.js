710036a3e347b9f0961bbc551f65c08bb7b6bd7a
Sujeong Ji <suji.sujeongji@gmail.com>
2022-07-20 23:54:16 +0900

    add Link

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import './Home.css';
 import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
